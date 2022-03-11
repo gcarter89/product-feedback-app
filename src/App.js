b682e8f00d7fa6b0ff0dd3904720326816b74d46
Gareth Carter <gareth.carter@outlook.com>
2022-03-11 11:30:51 +0000

    implemented basic subheader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header.js';
+import Subheader from './components/Subheader/Subheader';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Subheader />
         {/* <Button backgroundColor='foxtrot' textColor='dark' size='small' /> */}
     </div>
   );
---
+++ src/components/Subheader/Subheader.js
@@ -0,0 +1,9 @@
+import styles from './subheader.module.scss';
+
+export default function Subheader() {
+    return (
+        <div className={styles.subheader}>
+        vv
+        </div>
+    )
+}
