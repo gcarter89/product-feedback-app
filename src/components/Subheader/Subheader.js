4272743b6b2bff7bfaa7ba535915e18d9c4484cd
Gareth Carter <gareth.carter@outlook.com>
2022-03-13 02:05:35 +0000

    added dropdown and subheader

--- src/components/Subheader/Subheader.js
+++ src/components/Subheader/Subheader.js
@@ -1,9 +1,26 @@
 import styles from './subheader.module.scss';
+import {ReactComponent as UpIcon} from '../../assets/shared/icon-arrow-down.svg';
 
 export default function Subheader() {
     return (
         <div className={styles.subheader}>
-        vv
+            <div className={styles.subheader_dropdown}>
+                <p className='_body3'>Sort by : <strong>Most Upvotes <UpIcon /></strong></p>
+                {/* <ul>
+                    <li>
+                        <p className='_body1'>Most Upvotes</p>
+                    </li>
+                    <li>
+                        <p className='_body1'>Least Upvotes</p>
+                    </li>
+                    <li>
+                        <p className='_body1'>Most Comments</p>
+                    </li>
+                    <li>
+                        <p className='_body1'>Least Comments</p>
+                    </li>
+                </ul> */}
+            </div>
         </div>
     )
 }
