7dc55a57dc0f316fb1d1d6bb4170b686ef80f647
Gareth Carter <gareth.carter@outlook.com>
2022-03-17 02:04:44 +0000

    added data json and implemented basic filter component

--- src/components/Subheader/Subheader.js
+++ src/components/Subheader/Subheader.js
@@ -1,25 +1,39 @@
 import styles from './subheader.module.scss';
 import {ReactComponent as UpIcon} from '../../assets/shared/icon-arrow-down.svg';
+import {ReactComponent as CheckIcon} from '../../assets/shared/icon-check.svg';
+import { useState } from 'react';
 
 export default function Subheader() {
+
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const filterList = ['Most Upvotes', 'Least Upvotes', 'Most Comments', 'Least Comments'];
+
+    function handleClick(event, index) {
+        event.preventDefault();
+        setSelectedIndex(index);
+        setIsFilterOpen(false);
+    }
+
     return (
         <div className={styles.subheader}>
             <div className={styles.subheader_dropdown}>
-                <p className='_body3'>Sort by : <strong>Most Upvotes <UpIcon /></strong></p>
-                {/* <ul>
-                    <li>
-                        <p className='_body1'>Most Upvotes</p>
-                    </li>
-                    <li>
-                        <p className='_body1'>Least Upvotes</p>
-                    </li>
-                    <li>
-                        <p className='_body1'>Most Comments</p>
-                    </li>
-                    <li>
-                        <p className='_body1'>Least Comments</p>
-                    </li>
-                </ul> */}
+                <p className='_body3'>Sort by : <strong className={styles.subheader_selector} onClick={() => setIsFilterOpen(!isFilterOpen)}>{filterList[selectedIndex]} <UpIcon /></strong></p>
+                {isFilterOpen &&
+                    <ul className={styles.subheader_selectionMenu}>
+                        {filterList.map((elem, index) => {
+                            return (
+                                <li key={index} onClick={(e) => handleClick(e, index)}>
+                                    {selectedIndex === index ? 
+                                        <><p  className='_body1'>{elem}</p> <CheckIcon /></>
+                                        : <p className='_body1'>{elem}</p>}
+                                    
+                                    
+                                </li>
+                            )
+                        })}
+                    </ul>}
             </div>
         </div>
     )
