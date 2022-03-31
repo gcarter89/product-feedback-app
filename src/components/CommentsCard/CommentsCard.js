984099e35e1ee5455b803e3bd5ae5cc905f71950
Gareth Carter <gareth.carter@outlook.com>
2022-03-31 18:42:36 +0100

    added reply functionality

--- src/components/CommentsCard/CommentsCard.js
+++ src/components/CommentsCard/CommentsCard.js
@@ -1,35 +1,19 @@
 import styles from './commentscard.module.scss';
+import { Comment } from './_subcomponents/Comment.js';
 
 export default function CommentsCard({data}) {
-    const image = require('../../assets/user-images/image-suzanne.jpg');
     console.log(data)
 
-
-
-    // const dataImage = require(dataImagePath);
-
-    // const children = data.map((elem, index) => {
-    //     return (
-            
-    //     )
-    // })
+    const children = data.map((elem, index) => {
+        return (
+            (data.length === index + 1) ? <Comment key={index} comment={elem} /> : <Comment key={index} comment={elem} border={true} />
+        )
+    })
 
     return (
         <div className={styles.commentsCard}>
             <h3 className={styles.commentsCard_commentCount}>{data.length} Comments</h3>
-            <div className={styles.commentsCard_commentContainer}>
-                <div className={styles.commentsCard_avatarHandleReply}>
-                    <div className={styles.commentsCard_avatarHandle}>
-                        <img src={image} alt={`avatar of ${data[0].user.name}`} />
-                        <div className={styles.commentsCard_handle}>
-                            <h4>{data[0].user.name}</h4>
-                            <p className={`_body3`}>@{data[0].user.username}</p>
-                        </div>
-                    </div>
-                    <h4 className={styles.commentsCard_reply}>Reply</h4>
-                </div>
-                <p className={`_body3 ${styles.commentsCard_commentBody}`}>{data[0].content}</p>
-            </div>
+            {children}
         </div>
     )
 }
