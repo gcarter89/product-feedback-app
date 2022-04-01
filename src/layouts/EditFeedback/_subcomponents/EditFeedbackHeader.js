a4d0f055d240d842affe91590fa4c7ee10c9cef1
Gareth Carter <gareth.carter@outlook.com>
2022-04-02 00:28:32 +0100

    finished edit feedback built out

--- src/layouts/EditFeedback/_subcomponents/EditFeedbackHeader.js
+++ src/layouts/EditFeedback/_subcomponents/EditFeedbackHeader.js
@@ -3,12 +3,13 @@ import {ReactComponent as LeftArrow} from '../../../assets/shared/icon-arrow-lef
 
 
 export default function EditFeedbackHeader({handle}) {
+    //may create general component for this as it's the same as addFeedback header.
     return (
         <header className={styles.header}>
-        <button onClick={(e) => handle(e)} className={styles.header_backButton}>
-            <LeftArrow className={styles.header_buttonIcon} />
-            <h4>Go Back</h4>
-        </button>
-</header>
+            <button onClick={(e) => handle(e)} className={styles.header_backButton}>
+                <LeftArrow className={styles.header_buttonIcon} />
+                <h4>Go Back</h4>
+            </button>
+        </header>
     )
 }
