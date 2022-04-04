d5fca16ac4b23862e93da54f271e45db2482097b
Gareth Carter <gareth.carter@outlook.com>
2022-04-04 22:51:42 +0100

    removed comments

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ import Roadmap from './layouts/Roadmap/Roadmap.js'
 
 function App() {
 
-    //to-do
-    //1. look at the state and refactor for each specific layout
-    //2. preserve the global state but make sure the localised state is preserved in layout components.
-
-
-    //miscellaneous array props
     const statusArray = [['Planned', jsonData.productRequests.filter(elem => elem.status === 'planned').length, 'Ideas prioritized for research'], ['In-Progress', jsonData.productRequests.filter(elem => elem.status === 'in-progress').length, 'Currently in Development'], ['Live', jsonData.productRequests.filter(elem => elem.status === 'live').length, 'Released features']];
 
 
@@ -26,21 +20,15 @@ function App() {
     const [feedbackDetailVisible, setFeedbackDetailVisible] = useState(false);
 
     //selection state variables
-
     const [selectedFeedbackDetail, setSelectedFeedbackDetail] = useState(jsonData.productRequests[0]);
-
-
-
-
-
 
     return (
         <div className="App">
             {/* <NewFeedback /> */}
             {/* <EditFeedback /> */}
             {/* <FeedbackDetail selectedFeedbackDetail={selectedFeedbackDetail} /> */}
-            {/* <Suggestions data={jsonData} setSelectedFeedbackDetail={setSelectedFeedbackDetail} setFeedbackDetailVisible={setFeedbackDetailVisible} statusArray={statusArray} setCardsVisible={setCardsVisible} setRoadmapVisible={setRoadmapVisible} /> */}
-            <Roadmap data={jsonData} setRoadmapVisible={setRoadmapVisible} setCardsVisible={setCardsVisible} statusArray={statusArray} />
+            <Suggestions data={jsonData} setSelectedFeedbackDetail={setSelectedFeedbackDetail} setFeedbackDetailVisible={setFeedbackDetailVisible} statusArray={statusArray} setCardsVisible={setCardsVisible} setRoadmapVisible={setRoadmapVisible} />
+            {/* <Roadmap data={jsonData} setRoadmapVisible={setRoadmapVisible} setCardsVisible={setCardsVisible} statusArray={statusArray} /> */}
         </div>
     );
 }
