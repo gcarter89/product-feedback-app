8692f0fd26132dd41bc712cd9433d42171611b3f
Gareth Carter <gareth.carter@outlook.com>
2022-04-19 12:35:33 +0100

    added some validation for comment form

--- src/layouts/FeedbackDetail/_subcomponents/CommentForm.js
+++ src/layouts/FeedbackDetail/_subcomponents/CommentForm.js
@@ -1,18 +1,19 @@
 import styles from './commentform.module.scss';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 export default function CommentForm({data, setData, id}) {
 
     const [characterCount, setCharacterCount] = useState(250);
     const [content, setContent] = useState('');
-    const ref = useRef(null);
-
+    const [contentError, setContentError] = useState(false);
 
     function handleSubmit(event, content, data, id) {
         event.preventDefault();
         let commentCount = 0;
         let commentObject = {};
         let index;
+
+        if (content.length === 0) return setContentError(true);
 
         commentObject.id = commentCount + 1;
         commentObject.content = content;
@@ -36,9 +37,9 @@ export default function CommentForm({data, setData, id}) {
         data.productRequests[index].comments.push(commentObject);
         setData({ ...data});
 
-        ref.current.value = ''
         setCharacterCount(250);
         setContent('');
+        setContentError(false);
     }
 
 
@@ -58,7 +59,8 @@ export default function CommentForm({data, setData, id}) {
     return (
         <div className={styles.commentForm}>
             <h3 className={styles.commentForm_title}>Add Comment</h3>
-            <textarea ref={ref} onChange={(e) => handleChange(e)} className={styles.commentForm_input} placeholder="Type your comment here" />
+            <textarea onChange={(e) => handleChange(e)} className={styles.commentForm_input} value={content.length > 0 ? content : ''} placeholder="Type your comment here" />
+            {contentError &&<p className={`_body3 ${styles.commentForm_error}`}>Can't be empty</p>}
             <div className={styles.commentForm_bodyButton}>
                 <p className={`_body3 ${styles.commentForm_body}`}>{characterCount} Characters left</p>
                 <button onClick={(e) => handleSubmit(e, content, data, id)} className={styles.commentForm_button}><h4>Post Comment</h4></button>
